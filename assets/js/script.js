c317e86407f39926af9c390cf64482f377aa5228
Richy Rich <richardflores009@gmail.com>
2020-09-03 12:34:44 -0700

    added loop that pulls values from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,30 @@ var humi = document.querySelector('.humidity');
 var wind = document.querySelector('.wind-speed');
 var uv = document.querySelector('.uv');
 var weatherarr = []
-var mainArr = []
 
-//grab specific attributes from weather api
-//
+
 
 var loadWeather = function(name, value){
     var test = localStorage.setItem(name, JSON.stringify(value))
-
-
-}
+    
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+        console.log( localStorage.getItem( localStorage.key( i ) ) );
+        console.log(localStorage.key( i ))
+      }
+    }
 
 
 // 'https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&appid=f46fb8ed11ba9a0d7afe9d56cc76d028'
 var currentWeather = function () {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=f46fb8ed11ba9a0d7afe9d56cc76d028'
+
+
+
     // make a get request to url
     fetch(apiUrl)
         .then(function (response) {
             // request was successful
             if (response.ok) {
-                console.log(response);
                 response.json().then(function (data) {
                     // console.log(data)
                     var nameValue = data['name'];
@@ -36,10 +39,10 @@ var currentWeather = function () {
                     var windValue = data['wind']['speed']
                     var descValue = data['weather'][0]['main']
                     // var uvValue = data['']
-                    weatherarr.push(tempValue,humidValue,windValue,descValue)
+                    weatherarr = [tempValue,humidValue,windValue,descValue]
                     loadWeather(nameValue, weatherarr)
                      
-                     
+                    
                     name1.innerHTML = nameValue;
                     temp.innerHTML = "Temp: " + tempValue;
                     desc.innerHTML = "Description: " + descValue;
@@ -66,9 +69,7 @@ var cardWeather = function () {
         .then(function (response) {
             // request was successful
             if (response.ok) {
-                console.log(response);
                 response.json().then(function (data) {
-                    console.log(data)
                 });
             } else {
                 alert("Error: " + response.statusText);
